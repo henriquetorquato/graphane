068840a65ccb02dbd26c4deadfd9eada986b873a
Henrique Torquato <henriquet@take.net>
2021-03-30 19:27:59 -0300

    Create get executing path

--- Graphane/graphane.cpp
+++ Graphane/graphane.cpp
@@ -1,8 +1,26 @@
 #include "csv_graph_reader.h"
 #include <iostream>
+#include <Windows.h>
+
+const std::string TARGET_FILE = "n3e2.csv";
+const std::string DIRECTORY_NAME_SEPARATOR = "\\";
+
+// https://gist.github.com/karolisjan/f9b8ac3ae2d41ec0ce70f2feac6bdfaf
+static std::string GetExecutingPath()
+{
+	char buffer[MAX_PATH];
+	GetModuleFileNameA(NULL, buffer, MAX_PATH);
+	std::string::size_type pos = std::string(buffer).find_last_of("\\/");
+
+	return std::string(buffer).substr(0, pos);
+}
 
 int main()
 {
-    CsvGraphReader reader("D:\\repos\\graphane\\Debug\\n3e2.csv");
+	std::string path(GetExecutingPath());
+	path.append(DIRECTORY_NAME_SEPARATOR);
+	path.append(TARGET_FILE);
+
+    CsvGraphReader reader(path);
     Graph graph = reader.ReadGraph();
 }
